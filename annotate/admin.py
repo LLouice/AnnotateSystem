596e9f4f778d3b09f9ab9beca57027ee5cc4d9a5
LLouice <g13920228592@gamil.com>
2018-07-21 22:22:58 +0800

    添加 admin 配置

--- annotate/admin.py
+++ annotate/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Company, CompanySynonym, News, Patent, Tech, TechSynonym, Finance, KeywordsCompany, KeywordsTech
 
 # Register your models here.
+
+
+class KeywordsCompanyAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_labeled")
+    list_filter = ["is_labeled"]
+
+
+class KeywordsTechAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_labeled")
+    list_filter = ["is_labeled"]
+
+admin.site.register(KeywordsCompany, KeywordsCompanyAdmin)
+admin.site.register(KeywordsTech, KeywordsTechAdmin)
 admin.site.register((Company, CompanySynonym, News,
-                     Patent, Tech, TechSynonym, Finance, KeywordsCompany, KeywordsTech))
+                     Patent, Tech, TechSynonym, Finance))
